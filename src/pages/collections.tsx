8d9e8d950e4760ae4cfa8c4c1daf4f1a6f7f61b9
NotSiah <162050137+NotSiah@users.noreply.github.com>
2026-05-31 11:13:59 -0400

    removed cupid tee

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -37,7 +37,7 @@ const collections: Collection[] = [
         ],
         tags: ["tops"],
       },
-      {
+      /*{
         id: "k2",
         name: "Cupid Tee",
         desc: 'Don\'t let loyality become slavery.',
@@ -48,7 +48,7 @@ const collections: Collection[] = [
   
         ],
         tags: ["tops"],
-      },
+      },*/
     ],
 
      
